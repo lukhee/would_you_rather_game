f9bb876e871a29573f3a26cefbdbac137402aec7
balogun lukman <o.balogun@ymail.com>
2020-07-08 23:44:14 +0100

    chore: navbar adjustment

--- src/components/pages/layout/navBar.js
+++ src/components/pages/layout/navBar.js
@@ -15,42 +15,45 @@ const VerticalLine = styled.span`
 `;
 
 const Badge = styled.span`
-position: absolute;
-top: -7px;
-font-size: 10px;
-`
+  position: absolute;
+  top: -3px;
+  font-size: 10px;
+  right: -10%;
+`;
 
 const NavBar = ({
   auth: { user, isAuthenticated, loading },
   questions,
   logout,
 }) => {
-  let no_q_created, no_q_answered = 0
-  if(isAuthenticated){
-    no_q_created = questions.filter(q=>q.creator === user.id).length
-    no_q_answered = questions.filter(({answers}) => answers.some(item => [user.id].includes(item))).length
+  let no_q_created,
+    no_q_answered = 0;
+  if (isAuthenticated) {
+    no_q_created = questions.filter((q) => q.creator === user.id).length;
+    no_q_answered = questions.filter(({ answers }) =>
+      answers.some((item) => [user.id].includes(item))
+    ).length;
   }
   return (
     <NavBarDiv className='row justify-content-between p-2 px-4 border-bottom border-danger'>
       <div className='col-2 d-flex justify-content-between'>
-        <h2 className="my-auto"> Logo </h2>
-        <VerticalLine></VerticalLine>
+        <h2 className='my-auto font-weight-bold'> WYR </h2>
+        <VerticalLine />
       </div>
       {!loading && isAuthenticated && (
         <div className='d-flex justify-content-between col-10 my-auto'>
           <div className='d-flex'>
-            <p className='p-1 px-2 my-auto mr-2 shadow-sm bg-white rounded position-relative'>
-              Q-Ans<Badge className='badge badge-danger ml-1'>{no_q_answered}</Badge>
+            <p className='p-1 my-auto mx-2 shadow-sm bg-white rounded position-relative'>
+              Q-Ans
+              <Badge className='badge badge-danger ml-1'>{no_q_answered}</Badge>
             </p>
-            <p className='p-1 px-2 my-auto mr-2 shadow-sm bg-white rounded position-relative'>
+            <p className='p-1 my-auto mr-2 shadow-sm bg-white rounded position-relative'>
               Q-Crt
-              <Badge className='badge badge-danger ml-1'>
-                {no_q_created}
-              </Badge>
+              <Badge className='badge badge-danger ml-1'>{no_q_created}</Badge>
             </p>
           </div>
           <div className='d-flex'>
-            <p className='my-auto'> {user.name}</p>
+            <p className='my-auto name font-weight-bold'> {user.name}</p>
             <button
               className='btn btn-sm btn-outline-danger bg-white ml-2'
               onClick={logout}
